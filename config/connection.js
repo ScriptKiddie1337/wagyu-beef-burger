b240e08795531b8f57f2caf1865d4c236671e658
Script Kiddie <37413338+ScriptKiddie1337@users.noreply.github.com>
2018-08-23 17:46:35 -0700

    Heroku framework fixes

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Hostsmash12@",
-  database: "burgers_db"
-});
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -28,6 +11,17 @@ if (process.env.JAWSDB_URL) {
     database: 'burgers_db'
   });
 };
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
